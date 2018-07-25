91b341c5fd4caaa78e8b4450d74bc378abd33b31
Harsh Vardhan Pandey <37102432+hvpandey91@users.noreply.github.com>
2018-07-25 20:32:44 +0530

    Initial commit

---
+++ js/contact_me.js
@@ -0,0 +1,59 @@
+// Contact Form Scripts
+
+$(function() {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+
+            var enquiryRef = firebase.database().ref().child('Enquiries');
+            var newEnquiryRef = enquiryRef.child(guid());
+            newEnquiryRef.set({
+              Name:$("input#name").val(),
+              Email:$("input#email").val(),
+              Phone:$("input#phone").val(),
+              Message:$("textarea#message").val(),
+              timestamp:Date.now(),
+            });
+
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#success > .alert-success')
+                .append("<strong>Your message has been sent. </strong>");
+            $('#success > .alert-success')
+                .append('</div>');
+
+            //clear all fields
+            $('#contactForm').trigger("reset");
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
+
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
